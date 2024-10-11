9039f1403ef440f67dd6ba267951679449717a5a
ilaria.guarnieri <ilaria.guarnieri@supertosano.com>
2024-10-11 09:04:05 +0200

    add

--- config/database.php
+++ config/database.php
@@ -45,12 +45,11 @@ return [
 
         'mysql' => [
             'driver' => 'mysql',
-            'host' => env('DB_HOST', 'buyer_app_remote'),
-            // 'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '25060'),
-            'database' => env('DB_DATABASE', 'buyer_app'),
-            'username' => env('DB_USERNAME', 'buyer'),
-            'password' => env('DB_PASSWORD', 'supertosano2024ciao!'),
+            'host' => env('DB_HOST', '127.0.0.1'),
+            'port' => env('DB_PORT', '3306'),
+            'database' => env('DB_DATABASE', 'forge'),
+            'username' => env('DB_USERNAME', 'forge'),
+            'password' => env('DB_PASSWORD', ''),
             'unix_socket' => env('DB_SOCKET', ''),
             'charset' => 'utf8mb4',
             'collation' => 'utf8mb4_unicode_ci',
