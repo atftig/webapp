1e86e3a3906dcf4854d8d88835e50e81aae4483e
ilaria.guarnieri <ilaria.guarnieri@supertosano.com>
2024-10-21 10:09:52 +0200

    [add] input nascosto. cliccando 'altro' nel menu a tendina l'input viene mostrato quindi sarà possibile inserire l'insegna manualmente

--- resources/views/app/pv-page.blade.php
+++ resources/views/app/pv-page.blade.php
@@ -1,37 +1,62 @@
 @extends('layouts.app')
 
-@section('content')<!-- Bootstrap CSS -->
+@section('content')
+<!-- Bootstrap CSS -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
 
-    <div class="container mt-5">
-        <!-- <div class="text-center mb-2">
+<div class="container mt-5">
+    <!-- <div class="text-center mb-2">
             <img src="{{ asset('storage/logo/logo.png') }}" alt="Logo" class="img-fluid" style="max-width: 230px;">
         </div> -->
-        <form action="{{ route('store-pv') }}" method="POST">
-            @csrf
-            <div class="mb-3">
-                <label for="insegna" class="form-label" style="font-size: 1.8rem;">Scegli Insegna</label>
-                <select class="form-select" id="insegna" name="insegna" required style="border: 1px solid #666666; border-radius: 8px; height:50px">
-                    <option value="" disabled selected>Seleziona un'opzione</option>
-                    <option value="rossetto">Rossetto</option>
-                    <option value="galassia">Galassia</option>
-                    <option value="bennet">Bennet</option>
-                    <option value="coop">Coop</option>
-                    <option value="conad">Conad</option>
-                    <option value="prix">Prix</option>
-                    <option value="eurospin">Eurospin</option>
-                    <option value="esselunga">Esselunga</option>
-                    <option value="lidl">Lidl</option>
-                    <option value="md">MD</option>
-                    <option value="carrefour">Carrefour</option>
-                    <option value="pam">Pam</option>
-                </select>
-            </div>
-            <div class="mb-3">
-                <label for="pv" class="form-label" style="font-size: 1.6rem;">Scegli Punto Vendita</label>
-                <input type="text" class="form-control" id="pv" name="pv" placeholder="Inserisci punto vendita" style="border: 1px solid #666666; border-radius: 8px; height:50px" required>
-            </div>
-            <button type="submit" class="btn btn-primary">Conferma</button>
-        </form>
-    </div>
+    <form action="{{ route('store-pv') }}" method="POST">
+        @csrf
+        <div class="mb-3">
+        <label for="insegna" class="form-label" style="font-size: 1.8rem;">Scegli Insegna</label>
+            <select class="form-select" id="select-insegna" name="insegna" required
+                style="border: 1px solid #666666; border-radius: 8px; height:50px" onchange="toggleInsegnaInput(this)">
+                <option value="" disabled selected>Seleziona un'opzione</option>
+                <option value="rossetto">Rossetto</option>
+                <option value="galassia">Galassia</option>
+                <option value="bennet">Bennet</option>
+                <option value="coop">Coop</option>
+                <option value="conad">Conad</option>
+                <option value="prix">Prix</option>
+                <option value="eurospin">Eurospin</option>
+                <option value="esselunga">Esselunga</option>
+                <option value="lidl">Lidl</option>
+                <option value="md">MD</option>
+                <option value="carrefour">Carrefour</option>
+                <option value="pam">Pam</option>
+                <option value="altro">altro...</option>
+            </select>
+
+            <p class="mt-2">oppure</p>
+
+            <input type="text" class="form-control mt-2" id="input-insegna" name="insegna" placeholder="Inserisci insegna"
+            style="border: 1px solid #666666; border-radius: 8px; height:50px; display:none;" required>
+        </div>
+        <div class="mb-3">
+            <label for="pv" class="form-label" style="font-size: 1.6rem;">Scegli Punto Vendita</label>
+            <input type="text" class="form-control" id="pv" name="pv" placeholder="Inserisci punto vendita"
+                style="border: 1px solid #666666; border-radius: 8px; height:50px" required>
+        </div>
+        <button type="submit" class="btn btn-primary">Conferma</button>
+    </form>
+</div>
+
+
+<script>
+
+    function toggleInsegnaInput(select) {
+        var input = document.getElementById('input-insegna');
+        if (select.value === 'altro') {
+            input.style.display = 'block';
+            input.focus();
+        } else {
+            input.style.display = 'none';
+        }
+    }
+
+</script>
+
 @endsection
