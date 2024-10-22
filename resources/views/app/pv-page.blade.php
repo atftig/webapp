28e63d21590c62d42d0dbc90b3f49aba22162303
ilaria.guarnieri <ilaria.guarnieri@supertosano.com>
2024-10-22 08:11:23 +0200

    [add] input select con altre insegne disponibili

--- resources/views/app/pv-page.blade.php
+++ resources/views/app/pv-page.blade.php
@@ -12,7 +12,7 @@
         @csrf
         <div class="mb-3">
         <label for="insegna" class="form-label" style="font-size: 1.8rem;">Scegli Insegna</label>
-            <select class="form-select" id="select-insegna" name="insegna" required
+            <select class="form-select" id="insegna" name="insegna" required
                 style="border: 1px solid #666666; border-radius: 8px; height:50px" onchange="toggleInsegnaInput(this)">
                 <option value="" disabled selected>Seleziona un'opzione</option>
                 <option value="rossetto">Rossetto</option>
@@ -27,14 +27,28 @@
                 <option value="md">MD</option>
                 <option value="carrefour">Carrefour</option>
                 <option value="pam">Pam</option>
+                <option value="a&o">A&O</option>
+                <option value="todis">Todis</option>
+                <option value="sisa">Sisa</option>
+                <option value="tigros">Tigros</option>
+                <option value="aldi">Aldi</option>
+                <option value="auchan">Auchan</option>
+                <option value="penny">Penny Market</option>
+                <option value="despar">Despar</option>
+                <option value="famila">Famila</option>
+                <option value="poli">Poli</option>
+                <option value="martinelli">Martinelli</option>
                 <option value="altro">altro...</option>
             </select>
 
-            <p class="mt-2">oppure</p>
+            <p class="mt-2" id="oppure" style="display: none;">oppure</p>
 
-            <input type="text" class="form-control mt-2" id="input-insegna" name="insegna" placeholder="Inserisci insegna"
+            <input type="text" class="form-control mt-2" id="input-insegna" name="insegna_altro" placeholder="Inserisci insegna"
             style="border: 1px solid #666666; border-radius: 8px; height:50px; display:none;" required>
         </div>
+
+<!--------------------------- punto vendita -->
+
         <div class="mb-3">
             <label for="pv" class="form-label" style="font-size: 1.6rem;">Scegli Punto Vendita</label>
             <input type="text" class="form-control" id="pv" name="pv" placeholder="Inserisci punto vendita"
@@ -47,13 +61,19 @@
 
 <script>
 
-    function toggleInsegnaInput(select) {
+function toggleInsegnaInput(select) {
         var input = document.getElementById('input-insegna');
+        var oppure = document.getElementById('oppure');
         if (select.value === 'altro') {
             input.style.display = 'block';
+            oppure.style.display = 'block';
+            input.setAttribute('required', 'required'); // Imposta 'required' quando visibile
             input.focus();
         } else {
             input.style.display = 'none';
+            oppure.style.display = 'none';
+            input.removeAttribute('required'); // Rimuovi 'required' quando non visibile
+            input.value = ''; // Pulisci il campo se non è necessario
         }
     }
 
